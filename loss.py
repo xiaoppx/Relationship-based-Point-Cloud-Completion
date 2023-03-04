58e99b0c18036e71e8c8cf39dbe56eb0ad331b4d
zhangbowen-wx <zhangbowen.wx@gmail.com>
2023-03-04 12:11:18 +0800

    add loss

---
+++ loss.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+import emd
+import time
+import numpy as np
+
+
+def array2samples_distance(array1, array2):
+    """
+    arguments: 
+        array1: the array, size: (num_point, num_feature)
+        array2: the samples, size: (num_point, num_feature)
+    returns:
+        distances: each entry is the distance from a sample to array1 
+    """
+    num_point1, num_features1 = array1.shape
+    num_point2, num_features2 = array2.shape
+    expanded_array1 = array1.repeat(num_point2, 1)
+    expanded_array2 = torch.reshape(
+            torch.unsqueeze(array2, 1).repeat(1, num_point1, 1),
+            (-1, num_features2))
+    distances = (expanded_array1-expanded_array2)* (expanded_array1-expanded_array2)
+#    distances = torch.sqrt(distances)
+    distances = torch.sum(distances,dim=1)
+    distances = torch.reshape(distances, (num_point2, num_point1))
+    distances = torch.min(distances,dim=1)[0]
+    distances = torch.mean(distances)
+    return distances
+
+def chamfer_distance_numpy(array1, array2):
+    batch_size, num_point, num_features = array1.shape
+    dist = 0
+    for i in range(batch_size):
+        av_dist1 = array2samples_distance(array1[i], array2[i])
+        av_dist2 = array2samples_distance(array2[i], array1[i])
+        dist = dist + (0.5*av_dist1+0.5*av_dist2)/batch_size
+    return dist
+
+class cdModule(nn.Module):
+    def __init__(self):
+        super(cdModule,self).__init__()
+        
+    def forward(self,array1,array2):
+        return chamfer_distance_numpy(array1,array2)
+
+class emdFunction(Function):
+    @staticmethod
+    def forward(ctx, xyz1, xyz2, eps, iters):
+
+        batchsize, n, _ = xyz1.size()
+        _, m, _ = xyz2.size()
+
+        assert(n == m)
+        assert(xyz1.size()[0] == xyz2.size()[0])
+        assert(n % 1024 == 0)
+        assert(batchsize <= 512)
+
+        xyz1 = xyz1.contiguous().float().cuda()
+        xyz2 = xyz2.contiguous().float().cuda()
+        dist = torch.zeros(batchsize, n, device='cuda').contiguous()
+        assignment = torch.zeros(batchsize, n, device='cuda', dtype=torch.int32).contiguous() - 1
+        assignment_inv = torch.zeros(batchsize, m, device='cuda', dtype=torch.int32).contiguous() - 1
+        price = torch.zeros(batchsize, m, device='cuda').contiguous()
+        bid = torch.zeros(batchsize, n, device='cuda', dtype=torch.int32).contiguous()
+        bid_increments = torch.zeros(batchsize, n, device='cuda').contiguous()
+        max_increments = torch.zeros(batchsize, m, device='cuda').contiguous()
+        unass_idx = torch.zeros(batchsize * n, device='cuda', dtype=torch.int32).contiguous()
+        max_idx = torch.zeros(batchsize * m, device='cuda', dtype=torch.int32).contiguous()
+        unass_cnt = torch.zeros(512, dtype=torch.int32, device='cuda').contiguous()
+        unass_cnt_sum = torch.zeros(512, dtype=torch.int32, device='cuda').contiguous()
+        cnt_tmp = torch.zeros(512, dtype=torch.int32, device='cuda').contiguous()
+
+        emd.forward(xyz1, xyz2, dist, assignment, price, assignment_inv, bid, bid_increments, max_increments, unass_idx, unass_cnt, unass_cnt_sum, cnt_tmp, max_idx, eps, iters)
+
+        ctx.save_for_backward(xyz1, xyz2, assignment)
+        return dist, assignment
+
+    @staticmethod
+    def backward(ctx, graddist, gradidx):
+        xyz1, xyz2, assignment = ctx.saved_tensors
+        graddist = graddist.contiguous()
+
+        gradxyz1 = torch.zeros(xyz1.size(), device='cuda').contiguous()
+        gradxyz2 = torch.zeros(xyz2.size(), device='cuda').contiguous()
+
+        emd.backward(xyz1, xyz2, gradxyz1, graddist, assignment)
+        return gradxyz1, gradxyz2, None, None
+
+class emdModule(nn.Module):
+    def __init__(self):
+        super(emdModule, self).__init__()
+
+    def forward(self, input1, input2, eps, iters):
+        return emdFunction.apply(input1, input2, eps, iters)
+
+def test_emd():
+    x1 = torch.rand(20, 8192, 3).cuda()
+    x2 = torch.rand(20, 8192, 3).cuda()
+    emd = emdModule()
+    start_time = time.perf_counter()
+    dis, assigment = emd(x1, x2, 0.05, 3000)
+    print("Input_size: ", x1.shape)
+    print("Runtime: %lfs" % (time.perf_counter() - start_time))
+    print("EMD: %lf" % np.sqrt(dis.cpu()).mean())
+    print("|set(assignment)|: %d" % assigment.unique().numel())
+    assigment = assigment.cpu().numpy()
+    assigment = np.expand_dims(assigment, -1)
+    x2 = np.take_along_axis(x2, assigment, axis = 1)
+    d = (x1 - x2) * (x1 - x2)
+    print("Verified EMD: %lf" % np.sqrt(d.cpu().sum(-1)).mean())
+
+if __name__ == '__main__':
+    print()
+    # test_emd()
